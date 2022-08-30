4b05228b481b16371eb9898b0a5d667773f2f60d
ElianorKap <109879361+ElianorKap@users.noreply.github.com>
2022-08-30 20:49:02 +1200

    Added global variables to be input parameters for benchmarking

--- Main.py
+++ Main.py
@@ -1,17 +1,32 @@
 if __name__ == '__main__':
-    from benchmarking import *
-    from June_sdlab_functions import *
-    from JuneUncertainty import *
-    from lab2_curve_fitting import *
-    from Model_Unit_Tests import *
+
+# Global variables for benchmarking input paramters
+from q_array import *
+t , p = np.genfromtxt( 'gs_pres.txt' , delimiter=',', skip_header = 1 ).T
+a = 18.1
+B = 0.026
+p0 = 10.87
+q = find_q()
+    
 #1. Unit tests
+    from Model_Unit_Tests import *
     test_1()
     test_2()
     test_3()
     test_4()
+    
 # 2. Benchmarking
-    benchmarking()
+    from benchmarking import *
+    benchmarking(t, p, q, a, B, p0)
+                  
 # 3. Model Calibration
-    plot_kettle_model()
-# 4. Model Uncertainty
-    main()
+    from practice import *
+    plot_kettle_model() 
+    
+# 4. Predictions
+    from June_sdlab_functions import *
+    from JuneUncertainty import *
+    main() 
+    
+#5. Uncertainty
+    from Uncertainty import *
