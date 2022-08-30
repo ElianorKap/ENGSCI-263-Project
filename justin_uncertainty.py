34381e309dba14719b40d589f286de60c117ec14
ElianorKap <ekap183@aucklanduni.ac.nz>
2022-08-31 01:30:05 +1200

    Created uncertainty files

---
+++ justin_uncertainty.py
@@ -0,0 +1,192 @@
+
+
+# INSTRUCTIONS:
+# Jump to section "if __name__ == "__main__":" at bottom of this file.
+
+# import modules and functions
+import numpy as np
+from lumped_parameter_model import *
+from plotting import *
+
+def grid_search():
+	''' This function implements a grid search to compute the posterior over a and b.
+
+		Returns:
+		--------
+		a : array-like
+			Vector of 'a' parameter values.
+		b : array-like
+			Vector of 'b' parameter values.
+		P : array-like
+			Posterior probability distribution.
+	'''
+	# **to do**
+	# 1. DEFINE parameter ranges for the grid search
+	# 2. COMPUTE the sum-of-squares objective function for each parameter combination
+	# 3. COMPUTE the posterior probability distribution
+	# 4. ANSWER the questions in the lab document
+
+	# 1. define parameter ranges for the grid search
+	a_best = 18.1
+	b_best = 0.026
+
+	# number of values considered for each parameter within a given interval
+	N = 51	
+
+	# vectors of parameter values
+	a = np.linspace(a_best/2,a_best*1.5, N)
+	b = np.linspace(b_best/2,b_best*1.5, N)
+
+	# grid of parameter values: returns every possible combination of parameters in a and b
+	A, B = np.meshgrid(a, b, indexing='ij')
+
+	# empty 2D matrix for objective function
+	S = np.zeros(A.shape)
+
+	# data for calibration
+	tp,po = np.genfromtxt('gs_pres.txt', delimiter = ',')[:21,:].T
+
+	# error variance - 2 bar
+	v = 1.
+
+	# grid search algorithm
+	for i in range(len(a)):
+		for j in range(len(b)):
+			# 3. compute the sum of squares objective function at each value 
+			#pm =
+			#S[i,j] =
+			t, pm = solve_lpm(lpm, 2009, 2019, 0.1, 25.16, [10.87, a[i], b[j]])
+			p_model = [pm[i] for i in range(0,len(t)+1, 5 )]
+			S[i,j] = np.sum((po-p_model)**2)/v
+
+	# 4. compute the posterior
+	#P=
+	P = np.exp(-S/2.)
+
+	# normalize to a probability density function
+	Pint = np.sum(P)*(a[1]-a[0])*(b[1]-b[0])
+	P = P/Pint
+
+	# plot posterior parameter distribution
+	plot_posterior(a, b, P=P)
+
+	return a,b,P
+	
+####################################################################################
+#
+# Task3: Open fun_with_multivariate_normals.py and complete the exercises.
+#
+####################################################################################
+	
+####################################################################################
+#
+# Task 4: Sample from the posterior.
+#
+####################################################################################
+def construct_samples(a,b,P,N_samples):
+	''' This function constructs samples from a multivariate normal distribution
+	    fitted to the data.
+
+		Parameters:
+		-----------
+		a : array-like
+			Vector of 'a' parameter values.
+		b : array-like
+			Vector of 'b' parameter values.
+		P : array-like
+			Posterior probability distribution.
+		N_samples : int
+			Number of samples to take.
+
+		Returns:
+		--------
+		samples : array-like
+			parameter samples from the multivariate normal
+	'''
+	# **to do**
+	# 1. FIGURE OUT how to use the multivariate normal functionality in numpy
+	#    to generate parameter samples
+	# 2. ANSWER the questions in the lab document
+
+	# compute properties (fitting) of multivariate normal distribution
+	# mean = a vector of parameter means
+	# covariance = a matrix of parameter variances and correlations
+	A, B = np.meshgrid(a,b,indexing='ij')
+	mean, covariance = fit_mvn([A,B], P)
+
+	# 1. create samples using numpy function multivariate_normal (Google it)
+	#samples=
+	samples = np.random.multivariate_normal(mean, covariance, N_samples)
+
+	# plot samples and predictions
+	plot_samples(a, b, P=P, samples=samples)
+
+	return samples
+	
+####################################################################################
+#
+# Task 5: Make predictions for your samples.
+#
+####################################################################################
+def model_ensemble(samples):
+	''' Runs the model for given parameter samples and plots the results.
+
+		Parameters:
+		-----------
+		samples : array-like
+			parameter samples from the multivariate normal
+	'''
+	# **to do**
+	# Run your parameter samples through the model and plot the predictions.
+
+	# 1. choose a time vector to evaluate your model between 1953 and 2012 
+	# t =
+	t = np.linspace(2009, 2019, 101)
+
+	# 2. create a figure and axes (see TASK 1)
+	#f,ax =
+	f,ax = plt.subplots(1,1)
+
+	# 3. for each sample, solve and plot the model  (see TASK 1)
+	for a,b in samples:
+		#pm=
+		#ax.plot(
+		#*hint* use lw= and alpha= to set linewidth and transparency
+		t, pm = solve_lpm(lpm, 2009, 2019, 0.1, 25.16, [10.87, a, b])
+		ax.plot(t,pm,'k-', lw=0.25,alpha=0.2)
+	ax.plot([],[],'k-', lw=0.5,alpha=0.4, label='model ensemble')
+
+	# get the data
+	tp,po = np.genfromtxt('gs_pres.txt', delimiter = ',').T
+	ax.axvline(2009, color='b', linestyle=':', label='calibration/forecast')
+	
+	# 4. plot Wairakei data as error bars
+	# *hint* see TASK 1 for appropriate plotting commands
+	v = 2.
+	ax.errorbar(tp,po,yerr=v,fmt='ro', label='data')
+	ax.set_xlabel('time')
+	ax.set_ylabel('pressure')
+	ax.legend()
+	plt.show()
+
+if __name__=="__main__":
+	# Comment/uncomment each of the functions below as you complete the tasks
+	
+	# TASK 1: Read the instructions in the function definition.
+	#get_familiar_with_model()
+	
+	# TASK 2: Read the instructions in the function definition.
+	a,b,posterior = grid_search()
+	
+	# TASK 3: Open the file fun_with_multivariate_normals.py and complete the tasks.
+
+	# TASK 4: Read the instructions in the function definition.
+	# this task relies on the output of TASK 2, so don't comment that command
+	N = 100
+	samples = construct_samples(a, b, posterior, N)
+
+	# TASK 5: Read the instructions in the function definition.
+	# this task relies on the output of TASKS 2 and 3, so don't comment those commands
+	model_ensemble(samples)
+
+
---
+++ lumped_parameter_model.py
@@ -0,0 +1,128 @@
+
+#########################################################################################
+#
+# Function library for plotting lumped parameter model
+#
+# 	Functions:
+#		lmp_2D: lumped parameter model with 2 variables
+#		lmp_3D: lumped parameter model with 3 variables
+#		obj: objective function for a lmp
+#
+#########################################################################################
+
+# import modules and functions
+import numpy as np
+
+# global variables - observations
+tq,q = np.genfromtxt('gs_pres.txt', delimiter = ',').T
+tp,p = np.genfromtxt('gs_mass.txt', delimiter = ',').T
+dqdt = 0.*q                 # allocate derivative vector
+dqdt[1:-1] = (q[2:]-q[:-2])/(tq[2:]-tq[:-2])    # central differences
+dqdt[0] = (q[1]-q[0])/(tq[1]-tq[0])             # forward difference
+dqdt[-1] = (q[-1]-q[-2])/(tq[-1]-tq[-2])        # backward difference
+
+# define derivative function
+def lpm(t, p, q, p0, a, b):
+    overpressure = 25.6
+    if p >= overpressure:
+        # print(p)
+        return (a * q - b * (p - p0) ** 2)
+
+    return (a * q)
+
+# implement an improved Euler step to solve the ODE
+def solve_lpm(f, t0, t1, dt, x0, pars, scale=1.):
+    def step_ieuler(f, tk, yk, h, args=None, scale=1.):
+
+        # Solve q first
+        def qsolve1(tk, scale=1.):
+            q = interpolate_kettle_heatsource(tk, scale)
+            return q
+
+        if args is None:
+            args = []
+        # Find k1
+        q = qsolve1(tk, scale)
+        k1 = f(tk, yk, q, *args)
+        # Take a forward step
+        fake_step = yk + h * k1
+        # find k2
+        q = qsolve1(tk + h, scale)
+        k2 = f(tk + h, fake_step, q, *args)
+        # approximate solution value for next step
+        yk_1 = yk + 0.5 * h * (k1 + k2)
+        return yk_1
+
+    if pars is None:
+        pars = []
+
+    t = np.linspace(t0, t1, int((t1 - t0) / dt) + 1)
+
+    x = [x0]
+    t[0] = t0
+    t[-1] = t1
+    xstart = x0
+    for ind, i in enumerate(t):
+        # initial value already stored so can skip
+        if ind == 0:
+            continue
+        else:
+            # Uses improved euler
+            xk_1 = step_ieuler(f, i - dt, x0, dt, pars, scale)
+            x0 = xk_1
+            x.append(xk_1)
+
+    return t, x            # interp onto requested times
+
+def interpolate_kettle_heatsource(t, scale=1.):
+
+    time , mass = np.genfromtxt( 'gs_mass.txt' , delimiter=',', skip_header = 1 ).T
+    secondsPerMonth = 2628288 #average month
+    q = scale*mass / secondsPerMonth
+
+
+    for i in range(len(time)):
+        if t <= time[i]:
+            return q[i]
+
+def fit_mvn(parspace, dist):
+    """Finds the parameters of a multivariate normal distribution that best fits the data
+
+    Parameters:
+    -----------
+        parspace : array-like
+            list of meshgrid arrays spanning parameter space
+        dist : array-like 
+            PDF over parameter space
+    Returns:
+    --------
+        mean : array-like
+            distribution mean
+        cov : array-like
+            covariance matrix		
+    """
+    
+    # dimensionality of parameter space
+    N = len(parspace)
+    
+    # flatten arrays
+    parspace = [p.flatten() for p in parspace]
+    dist = dist.flatten()
+    
+    # compute means
+    mean = [np.sum(dist*par)/np.sum(dist) for par in parspace]
+    
+    # compute covariance matrix
+        # empty matrix
+    cov = np.zeros((N,N))
+        # loop over rows
+    for i in range(0,N):
+            # loop over upper triangle
+        for j in range(i,N):
+                # compute covariance
+            cov[i,j] = np.sum(dist*(parspace[i] - mean[i])*(parspace[j] - mean[j]))/np.sum(dist)
+                # assign to lower triangle
+            if i != j: cov[j,i] = cov[i,j]
+            
+    return np.array(mean), np.array(cov)
+        
